1f518a3f02fb6b2a741efb08b63f4557869d5178
halitklyc <halitenes.kalayci@gmail.com>
2025-05-06 20:43:34 +0300

    .

--- deep-learning/plate/main.py
+++ deep-learning/plate/main.py
@@ -4,3 +4,7 @@ from ultralytics import YOLO
 model = YOLO("yolov8n.pt")
 
 model.train(data="data.yaml", epochs=10, imgsz=640, batch=8)
+
+# bu modeli bütün veri setiyle eğitelim.
+
+# modeli nasıl kaydederiz? yeni img ile nasıl test ederiz?
