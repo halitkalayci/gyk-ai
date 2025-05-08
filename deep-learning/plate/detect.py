bf9128d00ffb1cb1fb1dbc2403f6882e317fefcc
halitklyc <halitenes.kalayci@gmail.com>
2025-05-08 19:15:30 +0300

    .

--- deep-learning/plate/detect.py
+++ deep-learning/plate/detect.py
@@ -2,7 +2,7 @@ import cv2
 import matplotlib.pyplot as plt
 
 
-img = cv2.imread("dataset/images/9.jpg")
+img = cv2.imread("dataset/images/1.jpg")
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 # Gürültü azaltma
--- deep-learning/plate/main.py
+++ deep-learning/plate/main.py
@@ -1,10 +1,20 @@
 # YoloV8
 from ultralytics import YOLO
+# yolov8n => bir computer vision modeli
 
-model = YOLO("yolov8n.pt")
+def train():
+    model = YOLO("yolov8n.pt")
+    model.train(data="data.yaml", epochs=10, imgsz=640, batch=8)
 
-model.train(data="data.yaml", epochs=10, imgsz=640, batch=8)
+# Pythonda starndart koruma yapısı.
+# python {main.py} => bu kodu çalıştırır.
+# ama diğer dosyalar import ettiğinde burayı çalıştırmaz.
+if __name__ == '__main__':
+    train()
 
 # bu modeli bütün veri setiyle eğitelim.
 
 # modeli nasıl kaydederiz? yeni img ile nasıl test ederiz?
+
+#GPU => Ekran kartı
+#CPU => işlemci
